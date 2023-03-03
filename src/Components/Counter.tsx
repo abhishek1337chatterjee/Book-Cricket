c9db5316a06db7e2a804844c3d272ac3818de565
abhishek1337chatterjee <abhishek1337chatterjee@gmail.com>
2023-03-03 23:01:29 +0530

    started working on countdown

---
+++ src/Components/Counter.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+export const Counter = () => {
+    interface CountDown {
+        seconds: number;
+    }
+  return (
+    <div>
+
+    </div>
+  )
+}
--- src/Pages/GamePage.tsx
+++ src/Pages/GamePage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const GamePage = () => {
   return (
     <div>
-        <h2>Timer Cricket</h2>
+
     </div>
   )
 }
