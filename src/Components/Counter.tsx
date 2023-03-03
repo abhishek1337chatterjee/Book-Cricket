083e33e75b30e92b3db2b0ee6f487d4e7d38a41f
abhishek1337chatterjee <abhishek1337chatterjee@gmail.com>
2023-03-04 01:42:48 +0530

    started working on countdown

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 
 
 export const Counter = () => {
-    interface CountDown {
-        seconds: number;
+    const [countdown, setCountdown] = useState<Number>(60);
+   
+    const id = useRef<Number>();
+    
+
+    const handleTimer = () =>{
+        
     }
+
+    
+    
+    
+
+
   return (
     <div>
-
+        <h2><>{countdown}</></h2>
+        <button onClick={handleTimer} style={{border: "2px soli"}}>Click to start</button>
     </div>
   )
 }
--- src/Pages/GamePage.tsx
+++ src/Pages/GamePage.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Counter } from '../Components/Counter'
 
 export const GamePage = () => {
   return (
     <div>
-
+        <Counter/>
     </div>
   )
 }
